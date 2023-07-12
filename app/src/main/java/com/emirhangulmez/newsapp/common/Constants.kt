e289de8386032c08da83fe4774b04298a2172b73
emirhangulmez <emirhn507@gmail.com>
2023-07-12 18:45:01 +0300

    Add service constant values to Constants file

--- app/src/main/java/com/emirhangulmez/newsapp/common/Constants.kt
+++ app/src/main/java/com/emirhangulmez/newsapp/common/Constants.kt
@@ -2,4 +2,14 @@ package com.emirhangulmez.newsapp.common
 
 object Constants {
 
+    const val BASE_URL = "https://newsapi.org/"
+    const val TOP_HEADLINES_ENDPOINT = "v2/top-headlines"
+
+    const val COUNTRY_QUERY_NAME = "country"
+    const val COUNTRY_QUERY_VALUE = "tr"
+
+    const val API_KEY_QUERY_NAME = "apiKey"
+    const val API_KEY_QUERY_VALUE = "4a19e75f811b461991b826e71ede820d"
+
+
 }
